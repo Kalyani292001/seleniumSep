0717dedd559f5cbf39ec374babd0b5f9ae0ad4d9
29052001k <kalyanihadole2001@gmail.com>
2023-08-29 09:59:50 +0530

    added

---
+++ SeleniumSep/src/Selenium23/SetUp.java
@@ -0,0 +1,18 @@
+package Selenium23;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SetUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+//	System.setProperty("webdriver.chrome.driver", "C:\\Users\\hello\\Desktop\\chromedriver\\chromedriver.exe");
+//	ChromeDriver driver=new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hello\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+	driver.get("https://www.linkedin.com/in/kalyani-hadole-49620422a/");
+	}
+
+}

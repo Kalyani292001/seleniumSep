c7f344a6a01a3d78ff9fe3985235c84161647b1e
29052001k <kalyanihadole2001@gmail.com>
2023-09-02 18:09:26 +0530

    loginPage

---
+++ SeleniumSep/src/Selenium23/Login.java
@@ -0,0 +1,40 @@
+package Selenium23;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\hello\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+
+		WebElement user = driver.findElement(By.cssSelector("[id=\"user-name\"]"));
+
+		WebElement pass = driver.findElement(By.cssSelector("[id=\"password\"]"));
+		
+		WebElement login=driver.findElement(By.cssSelector("[id=\"login-button\"]"));
+		
+		user.sendKeys("standard_user");
+		pass.sendKeys("secret_sauce");
+		login.click();
+		
+		WebElement logo=driver.findElement(By.cssSelector("[class=\"app_logo\"]"));
+		boolean l=logo.isDisplayed();
+		
+		if(l) {
+			System.out.println("test case pass");
+		}else {
+			System.out.println("test case fail");
+		}
+		
+		driver.close();
+		}
+
+	}
+
+
--- SeleniumSep/src/Selenium23/SetUp.java
+++ SeleniumSep/src/Selenium23/SetUp.java
@@ -7,12 +7,12 @@ public class SetUp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
 //	System.setProperty("webdriver.chrome.driver", "C:\\Users\\hello\\Desktop\\chromedriver\\chromedriver.exe");
 //	ChromeDriver driver=new ChromeDriver();
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\hello\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
-		ChromeDriver driver=new ChromeDriver();
-	driver.get("https://www.linkedin.com/in/kalyani-hadole-49620422a/");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\hello\\Downloads\\chromedriver\\chromedriver\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.linkedin.com/in/kalyani-hadole-49620422a/");
 	}
 
 }
